43f63db71ce6deeb8928c5debe80c7e46b29e6b5
Vaishali Shivakumar <130084250+Vaishu1293@users.noreply.github.com>
2024-11-12 14:02:53 +0000

    discover

--- pages/components/NavBar/NavBar.jsx
+++ pages/components/NavBar/NavBar.jsx
@@ -22,6 +22,36 @@ const [notification, setNotification] = useState(false);
 const [profile, setProfile] = useState(false);
 const [openSideMenu, setOpenSideMenu] = useState(false);
 
+const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if(btnText == "Discover"){
+        setDiscover(true);
+        setHelp(false);
+        setNotification(false);
+        setProfile(false);
+    } else if (btnText == "Help Center") {
+        setDiscover(false);
+        setHelp(true);
+        setNotification(false);
+        setProfile(false);
+    } else {
+        setDiscover(false);
+        setHelp(false);
+        setNotification(false);
+        setProfile(false);
+    }
+}
+
+const openNotification = () => {
+    if(!notification){
+        setDiscover(false);
+        setHelp(false);
+        setNotification(true);
+        setProfile(false);
+    } else {
+        setNotification(false);
+    }
+}
 //  Video left at 32:48 video 3
 
   return (
@@ -39,9 +69,42 @@ const [openSideMenu, setOpenSideMenu] = useState(false);
             
             <div className='{Style.navbar_container_right}'>
                 <div className='{Style.navbar_container_right_discover'>
-                    <p onClick={(e) => {}}>Discover</p>
-                    <div className='{Style.navbar_container_right_discover_box}'>
+                    {/* DISCOVER MENU */}
+                    <p onClick={(e) => openMenu(e)}>Discover</p>
+                    {discover && (
+                        <div className='{Style.navbar_container_right_discover_box}'>
                         <Discover/>
+                    </div>
+                    )}
+                </div>
+
+                {/* HELP CENTER MENU */}
+                <div className='{Style.navbar_container_right_help'>
+                    <p onClick={(e) => openMenu(e)}>Help Center</p>
+                    {help && (
+                        <div className='{Style.navbar_container_right_help_box}'>
+                        <HelpCenter/>
+                    </div>
+                    )}
+                </div>
+
+                {/* NOTIFICATION */}
+                <div className='{Style.navbar_container_right_notify'>
+                    <MdNotifications className={Style.notify} onClick={() => openNotification()} />
+                    {notification && <Notification/>}
+                </div>
+
+                {/* CREATE BUTTON SECTION */}
+                <div className={Style.navbar_container_right_button}>
+                    <Button btnText="Create"/>
+                </div>
+
+                {/* USER PROFILE */}
+                <div className={Style.navbar_container_right_profile_box}>
+                    <div className={Style.navbar_container_right_profile}>
+                        <Image src={images.user1} alt="Profile" width={40} height={40}
+                        onClick={() => openProfile()}/>
+                        {/* stopeed at 40.44 */}
                     </div>
                 </div>
             </div>
