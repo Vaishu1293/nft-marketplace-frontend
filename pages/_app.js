5218fbf0813bc3e86b4d25eb524fba259a31aa67
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2024-11-22 07:32:07 +0000

    footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 
 import NavBar from "../pages/components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 const App = ({ Component, pageProps}) => (
   <div>
     <Component {...pageProps} />
     <NavBar />
+    <Footer/>
   </div>
 );
 
--- pages/components/Footer/Footer.jsx
+++ pages/components/Footer/Footer.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import Image from "next/image";
+import { 
+    TiSocialFacebook,
+    TiSocialLinkedin,
+    TiSocialTwitter,
+    TiSocialYoutube,
+    TiSocialInstagram,
+    TiArrowSortedDown,
+    TiArrowSortedUp
+  } from "react-icons/ti";
+import {RiSendPlaneFill} from 'react-icons/ri';
+
+//  Internal Import
 import Style from './Footer.module.css';
+import images from '../../../img';
+import Discover from "../NavBar/Discover/Discover";
+import HelpCenter from "../NavBar/HelpCenter/HelpCenter";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={Style.footer}>
+            <div className={Style.footer_box}>
+                <div className={Style.footer_box_social}>
+                    <Image src={images.logo} alt="footer_logo" height={100} width={100}/>
+                    <p>Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.
+                    </p>
+                    <div className={Style.footer_social}>
+                        <a href="#">
+                            <TiSocialFacebook/>
+                        </a>
+                        <a href="#">
+                            <TiSocialLinkedin/>
+                        </a>
+                        <a href="#">
+                            <TiSocialTwitter/>
+                        </a>
+                        <a href="#">
+                            <TiSocialYoutube/>
+                        </a>
+                        <a href="#">
+                            <TiSocialInstagram/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
